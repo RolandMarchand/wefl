2d967ff82e725609fec7d18097c3e2b13df302a3
Moowool <moowool195@gmail.com>
2021-11-17 20:35:54 -0500

    Added '_get_linedefs' and '_get_sidedefs' functions.

--- Node2D.gd
+++ Node2D.gd
@@ -4,28 +4,46 @@ enum {FROM, TO}
 
 func _ready():
 	yield(WAD, "data_ready")
-	print(_get_things("MAP01"))
+	print(_get_lindefs("MAP01"))
 
 ## Takes a map as a string, either in MAPxy or ExMy format.
 ## Returns an array of all THINGS of the map.
 ## Each thing is represented by a dictionary of its proprieties, x position,
 ## y position, the angle, the type and the flags.
+##
+## TODO, check input validity
 func _get_things(map: String) -> Array:
-	var things_data: Array = WAD.MAPS["MAP01"]["THINGS"]
-	var things_count = (things_data[TO] - things_data[FROM]) / 10 # A thing is always 10 bytes
+	# A thing is always 10 bytes long
+	var LENGTH := 10
+	# Array of int, indicating the location of the data in the WAD
+	var things_data_ptr: Array = WAD.MAPS[map]["THINGS"]
+	# A thing is always 10 bytes
+	var things_count = (things_data_ptr[TO] - things_data_ptr[FROM]) / LENGTH
+	# To be returned
 	var things_array: Array = []
 
-	if map.begins_with("M"):
+	if map.begins_with("M"): # MAPxy label
 		for i in range(things_count):
-			var pos = things_data[0] + (i * 10) # A thing is always 10 bytes long
+			# A thing is always 10 bytes long
+			var current_thing = things_data_ptr[0] + (i * LENGTH)
 
 			var things_buffer: Dictionary = {
-				# Each thing is a <short>, 2 bytes long
-				"x": _short(WAD.FILE.subarray(pos, pos + 1)),
-				"y": _short(WAD.FILE.subarray(pos + 2, pos + 3)),
-				"angle": _short(WAD.FILE.subarray(pos + 4, pos + 5)),
-				"type": _short(WAD.FILE.subarray(pos + 6, pos + 7)),
-				"flags": _short(WAD.FILE.subarray(pos + 8, pos + 9))
+				# Each field is a <short>, 2 bytes long
+				"x": _short(
+					WAD.FILE.subarray(
+						current_thing, current_thing + 1)),
+				"y": _short(
+					WAD.FILE.subarray(
+						current_thing + 2, current_thing + 3)),
+				"angle": _short(
+					WAD.FILE.subarray(
+						current_thing + 4, current_thing + 5)),
+				"type": _short(
+					WAD.FILE.subarray(
+						current_thing + 6, current_thing + 7)),
+				"flags": _short(
+					WAD.FILE.subarray(
+						current_thing + 8, current_thing + 9))
 			}
 
 			things_array.append(things_buffer)
@@ -34,12 +52,102 @@ func _get_things(map: String) -> Array:
 
 	return things_array
 
+## TODO, check input validity
+func _get_lindefs(map: String) -> Array:
+	# A linedef is always 14 bytes long
+	var LENGTH := 14
+	# Array of int, indicating the location of the data in the WAD
+	var linedefs_data_ptr: Array = WAD.MAPS["MAP01"]["LINEDEFS"]
+	var linedefs_count = (linedefs_data_ptr[TO] - linedefs_data_ptr[FROM]) / LENGTH
+	# To be returned
+	var linedefs_array: Array = []
 
-## Returns a short from a set of two bytes
+	if map.begins_with("M"): # MAPxy label
+		for i in range(linedefs_count):
+			var current_linedef = linedefs_data_ptr[0] + (i * LENGTH)
+
+			var linedef_buffer: Dictionary = {
+				# Each field is a <short>, 2 bytes long
+				"vertex_1": _short(
+					WAD.FILE.subarray(
+						current_linedef, current_linedef + 1)),
+				"vertex_2": _short(
+					WAD.FILE.subarray(
+						current_linedef + 2, current_linedef + 3)),
+				"flags": _short(
+					WAD.FILE.subarray(
+						current_linedef + 4, current_linedef + 5)),
+				"special_type": _short(
+					WAD.FILE.subarray(
+						current_linedef + 6, current_linedef + 7)),
+				"sector_tag": _short(
+					WAD.FILE.subarray(
+						current_linedef + 8, current_linedef + 9)),
+				"front_sidedef": _short(
+					WAD.FILE.subarray(
+						current_linedef + 10, current_linedef + 11)),
+				"back_sidedef": _short(
+					WAD.FILE.subarray(
+						current_linedef + 12, current_linedef + 13)),
+			}
+
+			linedefs_array.append(linedef_buffer)
+	else: # ExMy label, to be implemented
+		pass
+
+	return linedefs_array
+
+func _get_sidedefs(map: String) -> Array:
+	# A sidedef is always 30 bytes long
+	var LENGTH := 30
+	# Array of int, indicating the location of the data in the WAD
+	var sidedefs_data_ptr: Array = WAD.MAPS["MAP01"]["SIDEDEFS"]
+	var sidedefs_count = (sidedefs_data_ptr[TO] - sidedefs_data_ptr[FROM]) / LENGTH
+	# To be returned
+	var sidedefs_array: Array = []
+
+	if map.begins_with("M"): # MAPxy label
+		for i in range(sidedefs_count):
+			var current_sidedefs = sidedefs_data_ptr[0] + (i * LENGTH)
+
+			var sidedefs_buffer: Dictionary = {
+				# Each sidedef's record is 30 bytes,
+				# comprising 2 <short> fields,
+				# then 3 <8-byte string> fields,
+				# then a final <short> field
+				"x_offset": _short(
+					WAD.FILE.subarray(
+						current_sidedefs, current_sidedefs + 1)),
+				"y_offset": _short(
+					WAD.FILE.subarray(
+						current_sidedefs + 2, current_sidedefs + 3)),
+				"upper_texture": WAD.FILE.subarray(
+						current_sidedefs + 4, current_sidedefs + 11
+						).get_string_from_ascii(),
+				"lower_texture": WAD.FILE.subarray(
+						current_sidedefs + 12, current_sidedefs + 19
+						).get_string_from_ascii(),
+				"middle_texture": WAD.FILE.subarray(
+						current_sidedefs + 20, current_sidedefs + 27
+						).get_string_from_ascii(),
+				"front_sidedef": _short(
+					WAD.FILE.subarray(
+						current_sidedefs + 28, current_sidedefs + 29)),
+			}
+
+			sidedefs_array.append(sidedefs_buffer)
+	else: # ExMy label, to be implemented
+		pass
+
+	return sidedefs_array
+
+
+## Returns a pseudo-short from a set of two bytes
 func _short(bytes: PoolByteArray) -> int:
 	var sum: int = (bytes[1] << 8) + bytes[0]
 
 	if 1<<15 & sum: # If the short is negative
-		return (sum - 32768) * -1 # Flips last bit on set of two bytes
-	else:
-		return sum
+		if sum == 0xffff:
+			return 0xffff
+		return (sum - 0x8000) * -1 # Flips last bit on set of two bytes
+	return sum
